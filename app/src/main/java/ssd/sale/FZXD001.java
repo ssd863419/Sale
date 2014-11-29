0a074ce981a10fb5933f3b91cdd4a6cde196f737
0_o <summer.wxy@gmail.com>
2014-11-29 15:08:57 +0800

    modify FZXD001

--- app/src/main/java/ssd/sale/FZXD001.java
+++ app/src/main/java/ssd/sale/FZXD001.java
@@ -119,6 +119,7 @@ public class FZXD001 extends ListFragment implements Button.OnClickListener {
 
             if (convertView == null) {
                 convertView = mInflater.inflate(R.layout.fzxd001_item, null);
+                // add this line
                 holder = _.getViews(convertView);
 
                 convertView.setTag(holder);
